7e273a73a9b16091ada180e29ad3733af0307f47
imsuraj <surya.shah1414@gmail.com>
2024-02-11 22:04:53 +0545

    Updated video setting

--- cypress/e2e/my-tests/SalesInvoice/salesInvoiceTest.cy.js
+++ cypress/e2e/my-tests/SalesInvoice/salesInvoiceTest.cy.js
@@ -29,7 +29,7 @@ describe('Sales Invoice pages suite', () => {
 
     it.only('Verify Header of Sales Invoice Page', () => {
         cy.getHeaderText('headerText')
-        cy.get('@headerText').then(headerText => {
+        cy.get('@headerTexta').then(headerText => {
             try {
                 expect(headerText).to.eq('Sales Invoices')
             } catch (error) {
