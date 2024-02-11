edfb1790046237d1d62c9dccb6db037fee7f02ba
imsuraj <surya.shah1414@gmail.com>
2024-02-11 21:32:40 +0545

    Updated video setting

--- cypress/e2e/my-tests/SalesInvoice/salesInvoiceTest.cy.js
+++ cypress/e2e/my-tests/SalesInvoice/salesInvoiceTest.cy.js
@@ -27,9 +27,9 @@ describe('Sales Invoice pages suite', () => {
 
     })
 
-    it.only('Verify Header of Sales Invoice Page', () => {
+    it('Verify Header of Sales Invoice Page', () => {
         cy.getHeaderText('headerText')
-        cy.get('@headerTexta').then(headerText => {
+        cy.get('@headerText').then(headerText => {
             try {
                 expect(headerText).to.eq('Sales Invoices')
             } catch (error) {
@@ -38,7 +38,7 @@ describe('Sales Invoice pages suite', () => {
         })
     })
 
-    it.only('Verify Search By Cusomter Ledger', () => {
+    it('Verify Search By Cusomter Ledger', () => {
         onSalesInvoicePage.searchInvoiceByText("Jain Kirana Pasal")
     })
 
