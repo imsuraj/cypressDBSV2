d753a83ec2d32ad644a11977770968838a82fb66
imsuraj <surya.shah1414@gmail.com>
2024-02-15 18:39:58 +0545

    Updated list promotion tests

--- cypress/e2e/tests/Configuration/Others/Promotion/listPromotionTest.cy.js
+++ cypress/e2e/tests/Configuration/Others/Promotion/listPromotionTest.cy.js
@@ -82,7 +82,7 @@ describe("List Promotion Test", () => {
 
     })
 
-    it.only("Check if active Brands are displayed", () => {
+    it("Check if active Brands are displayed", () => {
         const desiredBrand = ['Pick list',"Vicks","50-50","Ariel","Gillette","Head & Shoulders","Herbal Essence","OLD SPICE","Olay","Old Spice","Oral B","Pampers","Pantene","Tide","Vicks","Whisper","Bourbon","Bravo","Cake","Digestive","Good Day","Island","Little Hearts","Marie Gold","Milk Bikis","Milk Rusk","Nutrichoice","Nutro","Rolls","Suji Toast","Tiger","Treat","Wheat Rusk","Juice","Mixed Juice","Inhaler","Parle","Kellogg's All Bran","Kellogg's Chocos","Kellogg's K Pak","Kellogg's Muesli","Kellogg's Oats","Skin Care","Others","Latest Brand","Red Bull","New Brand","Moto","Head & Shoulders","QA Brand","test-brand","check","Test","OBtest","Keshav","popo","QA","QA Brand","Nepali Brand","Collin","QA","nepali","Lenovo","bu1","Bu2","Packet","BU3","BOOST","HORLICKS","Promotional SKUs","VIVA","Local","Miscellaneous","New Brand","50-50","Cake","Digestive","Good Day","Little Hearts","Marie Gold","Milk Bikis","Milk Rusk","Nutrichoice","Suji Toast","Tiger","Treat","Wheat Rusk","Muna-Masala Tea","Muna-Normal Tea","Milk Chocolate","Dark chocolate","Choco lava","Fruits","VAT BU","Horlicks","waiwai","nails","Fruits"]
         // select title  from business_catalog_details bcd where catalog_id = 3 and active  order by lower(title), title ;
         onPromotionsPage.clickOnStandardFilter()
